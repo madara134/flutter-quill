08dd7782f4c87aac61b61f3acce7f3cd44214536
madara134 <madara134@gmail.com>
2024-07-12 15:22:00 +0700

    Revert "Try catch lỗi"
    This reverts commit 68a424fe00bf4bd0e4e940116d8908f3301813e4.

--- lib/src/utils/font.dart
+++ lib/src/utils/font.dart
@@ -21,33 +21,3 @@ dynamic getFontSize(dynamic sizeValue) {
   }
   return fontSize;
 }
-
-double? getFontSizeAsDouble(dynamic sizeValue,
-    {required DefaultStyles defaultStyles}) {
-  if (sizeValue is String &&
-      ['small', 'normal', 'large', 'huge'].contains(sizeValue)) {
-    return switch (sizeValue) {
-      'small' => defaultStyles.sizeSmall?.fontSize,
-      'normal' => null,
-      'large' => defaultStyles.sizeLarge?.fontSize,
-      'huge' => defaultStyles.sizeHuge?.fontSize,
-      String() => null,
-    };
-  }
-
-  if (sizeValue is double) {
-    return sizeValue;
-  }
-
-  if (sizeValue is int) {
-    return sizeValue.toDouble();
-  }
-
-  assert(sizeValue is String);
-  final fontSize = double.tryParse(sizeValue);
-  if (fontSize == null) {
-    return null;
-    // throw ArgumentError('Invalid size $sizeValue');
-  }
-  return fontSize;
-}
